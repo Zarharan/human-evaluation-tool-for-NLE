94f6b23a214ce56aa3c1e3b7139f1495d294782e
Majid Zarharan <zarharan.majid@gmail.com>
2023-12-08 11:53:37 +0330

    FEATURE: Import same instances for different models by using a static list of claim IDs

--- human_evaluation_tool/evaluation/import_data.py
+++ human_evaluation_tool/evaluation/import_data.py
@@ -9,23 +9,23 @@ from django.conf import settings
 
 def import_data():
 
-    args= {"target_path": "data/explanation/llj.csv", "seed": 14, "target_instance_no": 50}
+    args= {"target_path": "data/explanation/test.csv", "seed": 8, "target_instance_no": 50}
 
     target_file_df= pd.read_csv( os.path.join(settings.BASE_DIR, "evaluation/"+args["target_path"]))
-    target_file_df= target_file_df.dropna()
-    np.random.seed(args["seed"])
-    target_file_df= target_file_df.sample(n=args["target_instance_no"], random_state= args["seed"], weights = target_file_df.groupby('label')['label'].transform('count'))    
+    target_file_df= target_file_df[target_file_df['claim_id'].isin(settings.SELECTED_CLAIM_IDS)]
+
+    print("Veracity labels distribution: \n", target_file_df.groupby(['label'])['label'].count())
 
     explanation_col_title= ""
     for name in ["gpt3", "chat_gpt", "gpt4", "vicuna", "mistral", "falcon", "llama"]:
         if name in args["target_path"]:
-            if "explanation" in args["target_path"]:
+            if "explanation" in args["target_path"]: 
                 explanation_col_title= name
                 if name in ["vicuna", "mistral", "falcon", "llama"]:
                     explanation_col_title+= "_cleaned"
             elif "joint" in args["target_path"]:
                 explanation_col_title= name+ "_explanation"
-            break
+            break 
 
     for idx, row in target_file_df.iterrows():
         target_instance= Instance.objects.create(claim_id = row["claim_id"], claim = row["claim"], context = row["summarized_main_text"]
